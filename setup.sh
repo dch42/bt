343e37d74fa72493155a41b47b04e6ccd2dafd5a
CD <dch@dchair.local>
2022-02-25 11:36:47 -0600

    increase verbosity

--- setup.sh
+++ setup.sh
@@ -1,14 +1,24 @@
 #!/bin/sh
 
 declare personal_bin="/Users/$USER/bin" 
-declare cfg=".bash_profile" 
+declare cfg=".bash_profile"
+declare script="bt"
+
+declare grn="\e[0;92m"
+declare red="\e[0;91m"
+declare reset="\e[0m"
+
+printf "\nAttempting install of script '$script'...\n"
 
 [ -n $ZSH_VERSION ] && 
 cfg=".zprofile" 
 
 [[ -d $personal_bin ]] || 
 mkdir $personal_bin && 
+printf "Adding script '$script' to \$PATH in $cfg...\n" & 
 echo export PATH="$personal_bin:\$PATH" >> /Users/$USER/$cfg 
 
-chmod +x ./bt.sh &&
-cp ./bt.sh $personal_bin/bt
+chmod +x ./${script}.sh &&
+cp ./${script}.sh $personal_bin/bt &&
+printf "\n${grn}[SUCCESS]${reset} Script $script installed at '$personal_bin/$script'!\nInvoke with '$script -h' for more information.\n" ||
+printf "\n${red}[ERROR]${reset} Something went wrong...\n"
